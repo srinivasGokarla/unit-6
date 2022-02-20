945e86db30264ddfc188a2a9344f7667085b7ef3
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-02-20 23:57:31 +0530

    sun

--- coding/practice/src/App.js
+++ coding/practice/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import{useState} from "react"
 
 function App() {
- 
- 
+
+  const[state, setState] = useState(true)
+  
+if( state === false) {
+  return <button onClick={() => setState(true)}>Show</button>
+  
+}
   return (
     <div className="App">
-    
+      <h1>Srinivas</h1>
+      <button onClick={() => setState(false) }>Hide</button>
     </div>
   );
 }
