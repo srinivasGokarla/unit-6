3a4c5795ac655c91247c8bc9dda7395b3259211c
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-03-07 20:56:27 +0530

    one

--- dsa/spiral.js
+++ dsa/spiral.js
@@ -1,82 +1,37 @@
-function spiral(n,mat){
-    let mat1 = [];
-    let top = 0;
-    let left = 0;
-    let right = n-1;
-    let bottom = n-1;
-    let count = 0;
-    let size = n*n;
-    while (count < size) {
-        for(var cl = left; cl <= right && count <= size; cl++) {
-         // console.log(mat[r][left])
-          mat1.push(mat[bottom][cl])
-          count++;
-        }
-        bottom--;
-        for(var r = bottom; r >= top && count < size; r--) {
-         // console.log(mat[bottom][cl])
-          mat1.push(mat[r][right])
-          count++;
-        }
-        right--;
-        for(var cl = right; cl >= left && count < size; cl--) {
-         // console.log(mat[r][right])
-          mat1.push(mat[top][cl])
-          count++;
-      }
-      top++;
-    for(var r = top; r <= bottom ; r++) {
-     // console.log(mat[top][cl])
-      mat1.push(mat[r][left])
+function spiralTraversal(N, M, arr){
+    
+  var top = 0;
+  var left = 0;
+  var right = M-1; 
+  var bottom = N-1; 
+  var count = 0;
+  var size = N * M;
+  var mat1 = [];
+   while(count < size){
+    for(var r = top; r <= bottom && count < size; r++) {
+       mat1.push(arr[r][left]);
+       count++;
+}
+    left++;
+  
+    for(var cl = left; cl <= right && count < size; cl++){
+      mat1.push(arr[bottom][cl]);
       count++;
     }
-    top++;
-    }  
-    console.log(mat1.join(" "))
+    bottom--;
+
+    for(var r = bottom; r >= top && count < size; r--){
+      mat1.push(arr[r][right]);
+      count++;
     }
+    right--;
 
-
-
-function runProgram(input) {
-   input = input.trim().split("\n")
-   let test = +input[0];
-   let line = 1;
-   for(i=0;i<test;i++){
-       let n = +input[line++]
-       let mat = [];
-       for(j=0;j<n;j++){
-           mat.push(input[line++].trim().split(" ").map(Number));
-
-       }
-
-       spiral(n,mat)  
-   }
-    
+    for(var cl = right; cl >= left&& count < size; cl--){
+      mat1.push(arr[top][cl]);
+      count++;
+  }
+  top++;
 }
- 
+console.log(mat1.join(" "));
 
-   if (process.env.USERNAME === 'srini') {
-     runProgram(`1
-     3 
-     1 2 3
-     4 5 6
-     7 8 9
-     `)
-    } else {
-     process.stdin.resume();
-     process.stdin.setEncoding("ascii");
-     let read = "";
-     process.stdin.on("data", function (input) {
-       read += input;
-     });
-     process.stdin.on("end", function () {
-       read = read.replace(/\n$/, "");
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-     });
-     process.on("SIGINT", function () {
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-       process.exit(0);
-     });
-   }
+}
