8bb50775a2312141d56e9a3a56b2da7bc3c9c9cf
srinivasGokarla <87422030+srinu217@users.noreply.github.com>
2022-03-14 23:14:56 +0530

    mon

---
+++ dsa/bobbox.js
@@ -0,0 +1,38 @@
+
+function bob(N) {
+    if(N == 0) {
+        return 1;
+    }
+    if(N < 0) {
+        return 0;
+    } else {
+        return (bob(N-1) +  bob(N-3) + bob(N-5))
+    }
+}
+    function runProgram(input) {
+        input = input.trim().split("\n")
+        var N = Number(input[0])
+      
+        console.log(bob(N))
+      }
+
+if (process.env.USERNAME === "srini") {
+  runProgram(`7`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
---
+++ dsa/superpro.js
@@ -0,0 +1,10 @@
+// function sport(props) {
+//     return <li>{props.info}</li>
+
+// }
+// const games = ["aqua", "ind","mac"]
+
+// ReactDOM.render(element, document.getElementById('root'))
+const msSinceEpoch = (new Date("July 19, 2014")).getTime();
+const seventeenHoursLater = new Date(msSinceEpoch + 24*14 * 60 * 60 * 1000)
+console.log(seventeenHoursLater)
